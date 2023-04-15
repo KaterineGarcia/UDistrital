160530facb71b18f0f265235674436ca9ee6391b
Katerine13 <dayanakgarcia01@gmail.com>
2023-04-15 18:41:07 -0500

    Resuelto

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -8,5 +8,5 @@ vhe=vh*1.25 #valor hora extra
 bon=s_b*0.05 #bonificacion
 s_t=(s_b)+(vhe*he)+(bon*b) #salario total
 salario=s_t-((s_t*0.035)+(s_t*0.04)+(s_t*0.01))
-#se imprmie el salario
+#se imprime el salario
 print(round(salario,1))
