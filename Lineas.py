879f43a0e93825139c3e782a066aa3b59c63c999
KaterineGarcia <130000050+KaterineGarcia@users.noreply.github.com>
2023-04-30 11:14:26 -0500

    Add files via upload

---
+++ Lineas.py
@@ -0,0 +1,7 @@
+nom, num=input("Ingrese un nombre y un entero: ").split( " / ")
+num=int(num)
+print(nom,type(nom))
+print(num, type(num))
+
+for i in range(num):
+    print(nom,"\n") #\n significa salto de linea
